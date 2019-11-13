51346f271970825a719772d588f80d2b061a1b5f
kmcginley <kmcginley@invaio.com>
2019-11-13 17:33:23 -0500

    removed hardcoded barcode

--- Barcode Generation/BarcodeGenerator.py
+++ Barcode Generation/BarcodeGenerator.py
@@ -11,8 +11,8 @@ def generateBarcodeFile():
     
     ''' open last generated barcode file, and get the last used ID'''
     df = pd.read_csv('/Users/kmcginley/VL43/Laboratory Operations - Documents/Barcode Labels/SMP_Barcodes.csv')
-    #lastBarcode = df['Barcode'].iloc[0]
-    lastBarcode = 'SMP002100'
+    lastBarcode = df['Barcode'].iloc[0]
+    #lastBarcode = 'SMP002100'
     barcodeList = []
 
     x = 1
