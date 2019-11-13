bdb42b7deb55caac5df1989bd0558e381c091ebf
kmcginley <kmcginley@invaio.com>
2019-11-13 17:29:08 -0500

    Reverted back to accomodate rolls

--- Barcode Generation/BarcodeGenerator.py
+++ Barcode Generation/BarcodeGenerator.py
@@ -1,38 +1,40 @@
-'''Script to generate file for printing lab barcodes'''
-'''  This script assumes 23 pages of 80 labels '''
+'''Script to generate file for printing lab barcodes
+This will make an upload sheet for the Dymo printer 
+that will print 121 labels in replicates of 5. The IDs are
+printed in reverse order, so that the lowest number is the first one
+after wrapping around the spool'''
 
 import pandas as pd
 import os
 
 def generateBarcodeFile():
-    #os.chmod('/Users/kmcginley/VL43/Laboratory Operations - Documents/Barcode Labels/SMP_Barcodes.csv', 444)
+    
+    ''' open last generated barcode file, and get the last used ID'''
     df = pd.read_csv('/Users/kmcginley/VL43/Laboratory Operations - Documents/Barcode Labels/SMP_Barcodes.csv')
-    lastBarcode = df['Barcode'].iloc[0]
-    #lastBarcode = 'SMP002999'
+    #lastBarcode = df['Barcode'].iloc[0]
+    lastBarcode = 'SMP002100'
     barcodeList = []
 
     x = 1
-    for i in range(1,461):
-        for v in range(1,5):
+    for i in range(1,121):
+        for v in range(1,6):
             bc = 'SMP00'+ str(int(lastBarcode[-4::]) + x)
             barcodeList.append(bc)
         x += 1
 
     contents = pd.DataFrame({'Barcode':barcodeList})
+
     '''reverse the order of rows for label rolls'''
-    #contents = contents.iloc[::-1]
+    contents = contents.iloc[::-1]
     
+
     outFile = contents.to_csv('/Users/kmcginley/VL43/Laboratory Operations - Documents/Barcode Labels/SMP_Barcodes.csv', index=False)
     outFile2 = contents.to_csv('/Users/kmcginley/Documents/Programming/Personal/Python/Barcode Generation/SMP_Barcodes.csv', index=False)
     
     return outFile, outFile2
 
-# def change_perms():
-#     os.chmod('/Users/kmcginley/VL43/Laboratory Operations - Documents/Barcode Labels/SMP_Barcodes.csv', 0o444)
 
 
 if __name__ == '__main__':
     
     generateBarcodeFile()
-    
-   # change_perms()
